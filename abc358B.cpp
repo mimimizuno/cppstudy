a9b8d4968d7a30aa2898e66d0bd6a61309dabdda
mimimizuno <sukfishdry1217@gmail.com>
2024-06-19 15:36:18 +0900

    error

---
+++ abc358B.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N,A;
+    bool wait = false;
+    cin >> N >> A;
+    vector <int>t(N);
+    for(int i = 0;i < N;i++){
+        cin >> t.at(i);
+    }
+
+}
