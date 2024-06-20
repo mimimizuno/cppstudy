ad7ea28d1aa9902f920cd71a0c235f962ea9cb8a
mimimizuno <sukfishdry1217@gmail.com>
2024-06-20 17:33:25 +0900

    today's study

--- abc358C.cpp
+++ abc358C.cpp
@@ -1,5 +1,22 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
+
+std::vector<int> find_all(const std::string str, const std::string subStr)
+{
+    std::vector<int> result;
+
+    int subStrSize = subStr.size();
+    int pos = str.find(subStr);
+
+    while (pos != std::string::npos)
+    {
+        result.push_back(pos);
+        pos = str.find(subStr, pos + subStrSize);
+    }
+
+    return result;
+}
 
 int main()
 {
@@ -10,15 +27,49 @@ int main()
 
     cin >> n >> m;
     vector<string> s(n);
+    vector<string> find;
     for (int i = 0; i < n; n++)
     {
         string a;
         cin >> a;
-        s.at(i).append(a);
+        s[i].append(a);
     }
-    for (int i = 0; i < n; n++){
-        for (int j = 0;j < m;j++){
-            if(s.at(i).at(j) == "〇");
+    for (int i = 0; i < n; n++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            find = find_all(a," ");
         }
     }
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<string> s(n);
+    for (int i = 0; i < n; i++) cin >> s[i];
+    int ans = n;
+    // 選択肢の中からいくつか選んで最適なものを探す→bit全探索
+    for (int bit = 0; bit < (1 << n); bit++) {// bit全探索
+        vector<bool> exist(m);
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (bit & (1 << i)) {
+                ++cnt;
+                for (int j = 0; j < m; j++) {
+                    if (s[i][j] == 'o') exist[j] = true;
+                }
+            }
+        }
+        bool all_exist = true;
+        for (int j = 0; j < m; j++) {
+            if (!exist[j]) all_exist = false;
+        }
+        if (all_exist) ans = min(ans, cnt);
+    }
+    cout << ans << endl;
 }
