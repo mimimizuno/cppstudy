9d7e0c3b97f58521827e441dc70174bae4325c3c
mimimizuno <mizuno-soki-gb@ynu.jp>
2024-07-06 17:30:21 +0900

    7/6study

--- abc350c.cpp
+++ abc350c.cpp
@@ -5,20 +5,38 @@ int main(){
     int N;
     cin >> N;
     vector<int> A(N);
-    vector<bool> flg(N,false);
-    int a,tmp,jj;
+    int ans = 0;
+    // int a,tmp,jj;
     for(auto &x:A) cin >> x;
-    for(int i = 0;i < N;i++){
-        for(int j = i;j < N;j++){
-            int tmp = N+1;
-            if(A[j] < tmp){
-                tmp = A[j];
-                jj = j;
+    // for(int i = 0;i < N;i++){
+    //     for(int j = i;j < N;j++){
+    //         int tmp = N+1;
+    //         if(A[j] < tmp){
+    //             tmp = A[j];
+    //             jj = j;
+    //         }
+    //     }
+    //     a = A[i];
+    //     A[i] = tmp;
+    //     A[jj] = a;
+    //     cout << i << " " << jj << endl;
+    // }
+    while(true){
+        int i = 0;
+        int ans = 0;
+        while(i < N){
+            if(A[i] > A[i+1]){
+                cout << i+1 << " " << i+2 << endl;
+                swap(A[i],A[i+1]);
             }
+            if(A[i] == i+1){
+                ans++;
+            }
+            if(ans == N){
+                return 0;
+            }
+            i++;
         }
-        a = A[i];
-        A[i] = tmp;
-        A[jj] = a;
-        cout << i << " " << jj << endl;
+
     }
 }
